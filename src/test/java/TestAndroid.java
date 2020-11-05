eae20b1557e8d852ff5aaabc40570f4f337b27b6
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-05 12:47:21 +0300

    abc

--- src/test/java/TestAndroid.java
+++ src/test/java/TestAndroid.java
@@ -1,64 +1,45 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TestAndroid {
 
+    AndroidDriver<AndroidElement> driver;
+
     @Test
-    public static void test() throws MalformedURLException, InterruptedException {
-        AndroidDriver<AndroidElement> driver;
+    public void test() throws IOException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "android");
-        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("noReset", true);
 
-        capabilities.setCapability("deviceName", "Galaxy S6");
-        capabilities.setCapability("udid", "0915f9e4326d0805");
-        capabilities.setCapability("appPackage", "com.sec.android.app.popupcalculator");
-        capabilities.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
+        capabilities.setCapability("deviceName", "Xiaomi Redmi 7");
+        capabilities.setCapability("udid", "a6eaa0e2");
+        capabilities.setCapability("app","/home/mitrixi/Local_C/IdeaProjects/untitled/src/main/resources/vitrina-app-debug.apk");
 
-//        capabilities.setCapability("deviceName", "Xiaomi Redmi 7");
-//        capabilities.setCapability("udid", "a6eaa0e2");
-//        capabilities.setCapability("appPackage", "com.miui.calculator");
-//        capabilities.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
 
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+//        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
+//        driver = new AndroidDriver<>(new URL("http://192.168.99.103:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        MobileElement buttonDigit5 = driver.findElementById("com.sec.android.app.popupcalculator:id/bt_05");
-        MobileElement buttonMultiplication = driver.findElementById("com.sec.android.app.popupcalculator:id/bt_mul");
-        MobileElement buttonEquals = driver.findElementById("com.sec.android.app.popupcalculator:id/bt_equal");
-        MobileElement turnDisplay = driver.findElementById("com.sec.android.app.popupcalculator:id/rotation_button");
-        MobileElement txtCalc = driver.findElementById("com.sec.android.app.popupcalculator:id/txtCalc");
-        MobileElement btnBackspace = driver.findElementById("com.sec.android.app.popupcalculator:id/bt_backspace");
+        MobileElement inputField = driver.findElementById("ru.lyubimov.sdktestapp:id/configUrl");
+        inputField.sendKeys("http://10.254.7.131/");
+
+        MobileElement btnSubmit = driver.findElementById("ru.lyubimov.sdktestapp:id/submit");
+        btnSubmit.click();
 
 
-//        MobileElement buttonDigit5 = driver.findElementById("com.miui.calculator:id/btn_5_s");
-//        MobileElement buttonMultiplication = driver.findElementById("com.miui.calculator:id/btn_mul_s");
-//        MobileElement buttonEquals = driver.findElementById("com.miui.calculator:id/btn_equal_s");
-//        MobileElement tabFinance = driver.findElementById("com.miui.calculator:id/iv_tab_finance");
-//        MobileElement btnInvestment = driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[1]");
+        //смотрим, пришла ли json с https://stage.mediavitrina.ru/testdata/1223
 
-        txtCalc.sendKeys("10");
-        buttonMultiplication.click();
-        txtCalc.sendKeys("10");
-        buttonEquals.click();
-
-        new Actions(driver).clickAndHold(btnBackspace).perform();
-
-        buttonDigit5.click();
-        buttonMultiplication.click();
-        buttonDigit5.click();
-        buttonEquals.click();
-        turnDisplay.click();
-        Thread.sleep(2000);
-        turnDisplay.click();
     }
+
+
 }
