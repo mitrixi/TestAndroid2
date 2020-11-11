5813887a4a92e4a814fe9ec9421443f98b26e208
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-11 15:34:38 +0300

    рабочая версия перед демонстрацией работы 4

--- src/test/java/TestDevice.java
+++ src/test/java/TestDevice.java
@@ -58,8 +58,8 @@ public class TestDevice {
         capabilities.setCapability("app", this.getClass().getClassLoader().getResource(VITRINA_APP_APK_FILE).getPath());
 
 
-//        driver = new AndroidDriver<>(new URL("http://10.254.0.131:4723/wd/hub"), capabilities);   //for Jenkins
-        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);      //for local PC
+        driver = new AndroidDriver<>(new URL("http://10.254.0.131:4723/wd/hub"), capabilities);   //for Jenkins
+//        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);      //for local PC
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 
