612d1f7828e3a13c476c04a07dc5d2b38a90348d
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-11 11:56:03 +0300

    Change IP address. One digit

--- src/test/java/TestAndroid.java
+++ src/test/java/TestAndroid.java
@@ -23,15 +23,17 @@ public class TestAndroid {
         capabilities.setCapability("deviceName", "Xiaomi Redmi 7");
         capabilities.setCapability("udid", "a6eaa0e2");
         capabilities.setCapability("app","/home/mitrixi/Local_C/IdeaProjects/untitled/src/main/resources/vitrina-app-debug.apk");
+//        capabilities.setCapability("app","/opt/vitrina-app-debug.apk");
 
 
-        driver = new AndroidDriver<>(new URL("http://10.254.0.131:4723/wd/hub"), capabilities);
-//        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver<>(new URL("http://10.254.0.131:4723/wd/hub"), capabilities);   //for Jenkins
+//        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);      //for local PC
 //        driver = new AndroidDriver<>(new URL("http://192.168.99.103:4723/wd/hub"), capabilities);
+//        driver = new AndroidDriver<>(new URL("http://192.168.99.100:2376/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         MobileElement inputField = driver.findElementById("ru.lyubimov.sdktestapp:id/configUrl");
-        inputField.sendKeys("http://10.254.7.131/");
+        inputField.sendKeys("http://10.254.0.131/");
 
         MobileElement btnSubmit = driver.findElementById("ru.lyubimov.sdktestapp:id/submit");
         btnSubmit.click();
