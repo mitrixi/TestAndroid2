8c7c0e19f72b9a222e7a848b8695b586ecca0a94
Andrew Nekrasov <anekrasov@mmtr.ru>
2020-11-23 13:46:26 +0300

    test iOS

--- src/test/resources/tshark_start_script.sh
+++ src/test/resources/tshark_start_script.sh
@@ -1,2 +1,4 @@
 #ssh mitrixi@10.254.0.131 'sudo killall tshark'   #for Jenkins
-ssh mmtr@10.254.7.106 'tshark -Y "ip.src == 92.223.99.99 && ip.dst == 10.254.7.106"'   #for local
+#ssh mmtr@10.254.7.106 'tshark -Y "ip.src == 92.223.99.99 && ip.dst == 10.254.7.106"'   #for local
+
+ssh mmtr@10.254.7.106 'ls'
