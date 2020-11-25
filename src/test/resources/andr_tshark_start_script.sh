f81ab8e716f21ccbeeee1a8e404de6b51b9e488d
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-25 15:11:49 +0300

    false

--- src/test/resources/andr_tshark_start_script.sh
+++ src/test/resources/andr_tshark_start_script.sh
@@ -1,2 +1,2 @@
 #sudo tshark -i enp2s0 -Y "ip.src == 92.223.99.99"k   #for local
-ssh root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y "(ip.src == 92.223.99.99 || ip.src == 178.176.158.69 || ip.src == 195.161.167.68) && ip.dst == 10.254.7.106"'   #for Jenkins
+ssh root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y "(ip.src == 92.223.99.99 || ip.src == 178.176.158.69 || ip.src == 195.161.167.68) && ip.dst == 10.254.0.131"'   #for Jenkins
