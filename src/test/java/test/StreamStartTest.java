8c3d6e9da9c63f009e32ec05a32b38e9b4c651e8
Andrew Nekrasov <anekrasov@mmtr.ru>
2020-11-26 19:25:11 +0300

    iOS

--- src/test/java/test/StreamStartTest.java
+++ src/test/java/test/StreamStartTest.java
@@ -37,7 +37,7 @@ public class StreamStartTest {
         Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
         BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
 
-        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.getTsharkStartBlackout(CONFIG_FILE_URL));
+        Process tsharkProcessBlackout = Runtime.getRuntime().exec("ssh -tt mmtr@10.254.7.106 '/usr/local/bin/tshark -Y \"tls.handshake.session_id && ip.dst == 10.254.7.106 && ip.src == 193.27.224.38\"'");
         BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
 
         device.stepOk();
