47d3b3e9363e1d0f026b173017b28bbc9393f86f
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-26 16:48:27 +0300

    first step2

--- src/test/resources/andr_tshark_blackout_sniffing.sh
+++ src/test/resources/andr_tshark_blackout_sniffing.sh
@@ -1,1 +1,1 @@
-ssh root@10.254.0.131 '/home/mitrixi/Local_C/IdeaProjects/untitled/src/test/resources/andr_tshark_sniff_blackouts.sh'
+ssh -t root@10.254.0.131 '/home/mitrixi/Local_C/IdeaProjects/untitled/src/test/resources/andr_tshark_sniff_blackouts.sh'
