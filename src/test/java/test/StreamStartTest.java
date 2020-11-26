71a2f2ffc0244aaff14c9d7de3460811fcf3431e
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-26 16:31:46 +0300

    first step2

--- src/test/java/test/StreamStartTest.java
+++ src/test/java/test/StreamStartTest.java
@@ -33,21 +33,18 @@ public class StreamStartTest {
 
         // Запускаем tshark, читаем из консоли Stream
         Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
-//        Process tsharkProcessStream = Runtime.getRuntime().exec("ssh root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y \"tls.handshake.session_id && ip.dst == 10.10.0.102\"'");
         BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
 
-//        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.startBlackoutSniffing());
-//        Process tsharkProcessBlackout = Runtime.getRuntime().exec("ssh -t root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y \"ip.dst == 10.10.0.102\"'");
-//        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
+        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.startBlackoutSniffing());
+        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
 
         device.stepOk();
 
         TimeUnit.SECONDS.sleep(15);
-//            Thread.sleep(15000);
+
         Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStream));
 
         TimeUnit.SECONDS.sleep(10);
-//        Thread.sleep(10000);
 
 
 //        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessBlackout));
@@ -56,9 +53,7 @@ public class StreamStartTest {
         boolean isStreamStart = false;
         String strStream;
         while (tsharkProcessStreamReader.ready()) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
             System.out.println("Внутри while потока");
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
             strStream = tsharkProcessStreamReader.readLine();
             if (strStream.contains(device.getIP()) && Arrays.stream(TEST_STREAM_IP).anyMatch(strStream::contains) && strStream.contains(START_STREAM_SERVER_MSG)) {
                 System.out.println(strStream);
@@ -67,22 +62,20 @@ public class StreamStartTest {
             }
         }
 
-//        boolean existBlackout = false;
-//        String strBlackout;
-//        while (tsharkProcessBlackoutReader.ready()) {
-//            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//            System.out.println("Внутри while блэкаутов");
-//            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//            strBlackout = tsharkProcessBlackoutReader.readLine();
-//            System.out.println(strBlackout);
-//            if (strBlackout.contains(device.getIP()) && strBlackout.contains(BLACKOUTS_IP) && strBlackout.contains(START_STREAM_SERVER_MSG)) {
-//                existBlackout = true;
-//                break;
-//            }
-//        }
+        boolean existBlackout = false;
+        String strBlackout;
+        while (tsharkProcessBlackoutReader.ready()) {
+            System.out.println("Внутри while блэкаутов");
+            strBlackout = tsharkProcessBlackoutReader.readLine();
+            System.out.println(strBlackout);
+            if (strBlackout.contains(device.getIP()) && strBlackout.contains(BLACKOUTS_IP) && strBlackout.contains(START_STREAM_SERVER_MSG)) {
+                existBlackout = true;
+                break;
+            }
+        }
 
         assertThat("Видеопоток отсутствует", isStreamStart, equalTo(true));
-//        assertThat("Блэкаут отсутствует", existBlackout, equalTo(true));
+        assertThat("Блэкаут отсутствует", existBlackout, equalTo(true));
 
 //        String patternServerHello = "\\s*(\\d+)\\s(\\d+\\.\\d{9}).+(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}).+(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}).*\\bServer Hello\\b.*";
 //        String blackouts = "\\\\\\\\";
--- src/test/resources/andr_tshark_start_script.sh
+++ src/test/resources/andr_tshark_start_script.sh
@@ -1,2 +1,1 @@
-#sudo tshark -i enp2s0 -Y "ip.src == 92.223.99.99"k   #for local
-ssh root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y "(ip.src == 92.223.99.99 || ip.src == 178.176.158.69 || ip.src == 195.161.167.68) && ip.dst == 10.10.0.102"'   #for Jenkins
+ssh -t root@10.254.0.131 '/usr/bin/tshark -i enp2s0 -Y "(ip.src == 92.223.99.99 || ip.src == 178.176.158.69 || ip.src == 195.161.167.68) && ip.dst == 10.10.0.102"'   #for Jenkins
