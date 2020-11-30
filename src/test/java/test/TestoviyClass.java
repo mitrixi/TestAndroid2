5f1ea19810f7bff843717c4151f1bd04fadc78c4
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-30 16:04:14 +0300

    первый степ с122первый степ с122

---
+++ src/test/java/test/TestoviyClass.java
@@ -0,0 +1,54 @@
+package test;
+
+import device.AndroidDevice;
+import device.IDevice;
+import device.IosDevice;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static test.TestUtils.readJsonFromUrl;
+
+public class TestoviyClass {
+    @Test
+    public void test() throws IOException {
+
+
+        IDevice device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
+
+        JSONObject jsonConfigFile = readJsonFromUrl("http://10.254.0.131/");
+
+        String urlBlackout = jsonConfigFile.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_api_url").toString();
+        JSONObject jsonBlackout = readJsonFromUrl(urlBlackout);
+        boolean broadcasting_allowed = Boolean.parseBoolean(jsonBlackout.getJSONArray("restrictions").getJSONObject(0).get("broadcasting_allowed").toString());
+        System.out.println(broadcasting_allowed);
+
+        device.allowBlackout(); // ToDo
+
+
+        JSONObject jsonConfigFile2 = readJsonFromUrl("http://10.254.0.131/");
+        String urlBlackout2 = jsonConfigFile2.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_api_url").toString();
+        JSONObject jsonBlackout2 = readJsonFromUrl(urlBlackout2);
+        boolean broadcasting_allowed2 = Boolean.parseBoolean(jsonBlackout2.getJSONArray("restrictions").getJSONObject(0).get("broadcasting_allowed").toString());
+        System.out.println(broadcasting_allowed2);
+
+        device.restrictBlackout(); // ToDo
+
+
+        JSONObject jsonConfigFile3 = readJsonFromUrl("http://10.254.0.131/");
+        String urlBlackout3 = jsonConfigFile3.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_api_url").toString();
+        JSONObject jsonBlackout3 = readJsonFromUrl(urlBlackout3);
+        boolean broadcasting_allowed3 = Boolean.parseBoolean(jsonBlackout3.getJSONArray("restrictions").getJSONObject(0).get("broadcasting_allowed").toString());
+        System.out.println(broadcasting_allowed3);
+
+        device.allowBlackout(); // ToDo
+
+        JSONObject jsonConfigFile4 = readJsonFromUrl("http://10.254.0.131/");
+        String urlBlackout4 = jsonConfigFile4.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_api_url").toString();
+        JSONObject jsonBlackout4 = readJsonFromUrl(urlBlackout4);
+        boolean broadcasting_allowed4 = Boolean.parseBoolean(jsonBlackout4.getJSONArray("restrictions").getJSONObject(0).get("broadcasting_allowed").toString());
+        System.out.println(broadcasting_allowed4);
+
+    }
+}
