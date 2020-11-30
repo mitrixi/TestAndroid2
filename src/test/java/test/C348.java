3c0fd947cba2f5d0bc9c85873cff32180f84f49b
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-30 14:42:44 +0300

    первый степ с122

--- src/test/java/test/C348.java
+++ src/test/java/test/C348.java
@@ -1,135 +1,135 @@
-package test;
-
-import device.AndroidDevice;
-import device.IDevice;
-import device.IosDevice;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import javax.inject.Inject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static test.TestUtils.*;
-import static test.TestUtils.getSecFromBoStr;
-
-public class C348 {
-    public final static String CONFIG_FILE_URL = "http://10.254.0.131/";
-    public final static String START_STREAM_SERVER_MSG = "Server Hello";
-    public final static String START_STREAM_CLIENT_MSG = "Client Hello";
-    public final static String[] TEST_STREAM_IP = {"92.223.99.99", "178.176.158.69", "195.161.167.68"}; // СТС ToDo изменить на динамический
-    public final static int SLEEP_TIME_STREAM = 10;
-    public final static int SLEEP_TIME_BLACKOUT = 40; // Todo это restrictionsPeriodSec * boSuccessCount + 10
-
-    @BeforeClass
-    public void preinstallations() throws IOException {
-        IDevice device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
-        device.allowBlackout(); // ToDo
-    }
-
-    @Test(alwaysRun = true)
-    public void c348() throws IOException, InterruptedException {
-        IDevice device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
-
-        /******** Step 1 ********/
-        device.allowBlackout(); // строчка потестить
-
-        device.stepToConfigUrl(CONFIG_FILE_URL);
-
-        // Запускаем tshark, читаем из консоли Stream
-        Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
-        BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
-
-//        Process tsharkProcessBlackout = Runtime.getRuntime().exec("ssh -tt mmtr@10.254.7.106 '/usr/local/bin/tshark -Y \"tls.handshake.session_id && ip.dst == 10.254.7.106 && (ip.src == 193.27.224.36 || ip.src == 193.27.224.37 || ip.src == 193.27.224.38)\"'");
-        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.getTsharkStartBlackout(CONFIG_FILE_URL));
-        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
-
-        device.stepOk();
-
-        TimeUnit.SECONDS.sleep(SLEEP_TIME_STREAM);
-
-        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStream));
-
-        TimeUnit.SECONDS.sleep(SLEEP_TIME_BLACKOUT);
-
-        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessBlackout));
-        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
-
-        device.stepCancelStream();
-
-        boolean isStreamStart = false;
-        String strStream;
-        while (tsharkProcessStreamReader.ready()) {
-            strStream = tsharkProcessStreamReader.readLine();
-            System.out.println(strStream); // tmp for test
-            if (strStream.contains(START_STREAM_SERVER_MSG)) {
-                isStreamStart = true;
-                break;
-            }
-        }
-
-        List<String> blackoutList = new ArrayList<>(); // используется для Step 2
-        String boLine;
-        while (tsharkProcessBlackoutReader.ready()) {
-            boLine = tsharkProcessBlackoutReader.readLine();
-            if (boLine.contains(START_STREAM_SERVER_MSG)) {
-                blackoutList.add(boLine.trim());
-            }
-        }
-
-        boolean existBlackout = blackoutList.isEmpty() ? false : true;
-
-        assertThat("C348_Step1: Видеопоток ОТСУТСТВУЕТ", isStreamStart, equalTo(true));
-        assertThat("C348_Step1: Запрос на restrictions_api_url НЕ отправляется (блэкауты)", existBlackout, equalTo(true));
-
-        /******** Step 2 ********/
-
-        int boSuccessCount = 2;
-        int secBoReqInterval = 15; // todo jsonConfigFile.getJSONObject("results").getJSONObject("sdk_config").get("restrictions_period_sec")
-        int secBoReqLag = 2;
-
-        // 1) Должно быть еще 2 успешных запроса. (сам факт)
-        boolean existTwoSuccessBoReq = false;
-        // 2) Запросы отправляются каждые 15 секунд
-        boolean isPeriodicityBoReq = true;
-
-        // tmp for test
-        System.out.println("Блэкауты:");
-        blackoutList.forEach(boString -> {
-            System.out.println(boString);
-        });
-        // tmp for test
-
-        if (!blackoutList.isEmpty()) {
-            int firstSecBoReq = getSecFromBoStr(blackoutList.get(0));
-            Set<Integer> secFromBoStrSet = new HashSet<>();
-            blackoutList.forEach(boString -> {
-                int secFromBoStr = getSecFromBoStr(boString);
-                if ((secFromBoStr - firstSecBoReq) <= (boSuccessCount * secBoReqInterval + secBoReqLag))
-                    secFromBoStrSet.add(secFromBoStr);
-            });
-
-            // 1)
-            if (secFromBoStrSet.size() > boSuccessCount)
-                existTwoSuccessBoReq = true;
-
-            // 2) Анализируем отправку только первых boSuccessCount запросов
-            int indexBoString = 0;
-            for (String boString : blackoutList) {
-                if ((indexBoString > 0 && indexBoString <= boSuccessCount) && !(getSecFromBoStr(boString) >= (firstSecBoReq + secBoReqInterval * indexBoString - secBoReqLag) && getSecFromBoStr(boString) <= (firstSecBoReq + secBoReqInterval * indexBoString + secBoReqLag))) {
-                    isPeriodicityBoReq = false;
-                }
-                indexBoString++;
-            }
-        }
-
-        assertThat("C348_Step2: Ещё двух запросов на блэкауты НЕТ", existTwoSuccessBoReq, equalTo(true));
-        assertThat("C348_Step2: Периодичность отправки запросов НАРУШЕНА", isPeriodicityBoReq, equalTo(true));
-    }
-}
-
+//package test;
+//
+//import device.AndroidDevice;
+//import device.IDevice;
+//import device.IosDevice;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//import javax.inject.Inject;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.*;
+//import java.util.concurrent.TimeUnit;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.equalTo;
+//import static test.TestUtils.*;
+//import static test.TestUtils.getSecFromBoStr;
+//
+//public class C348 {
+//    public final static String CONFIG_FILE_URL = "http://10.254.0.131/";
+//    public final static String START_STREAM_SERVER_MSG = "Server Hello";
+//    public final static String START_STREAM_CLIENT_MSG = "Client Hello";
+//    public final static String[] TEST_STREAM_IP = {"92.223.99.99", "178.176.158.69", "195.161.167.68"}; // СТС ToDo изменить на динамический
+//    public final static int SLEEP_TIME_STREAM = 10;
+//    public final static int SLEEP_TIME_BLACKOUT = 40; // Todo это restrictionsPeriodSec * boSuccessCount + 10
+//
+//    @BeforeClass
+//    public void preinstallations() throws IOException {
+//        IDevice device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
+//        device.allowBlackout(); // ToDo
+//    }
+//
+//    @Test(alwaysRun = true)
+//    public void c348() throws IOException, InterruptedException {
+//        IDevice device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
+//
+//        /******** Step 1 ********/
+//        device.allowBlackout(); // строчка потестить
+//
+//        device.stepToConfigUrl(CONFIG_FILE_URL);
+//
+//        // Запускаем tshark, читаем из консоли Stream
+//        Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
+//        BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
+//
+////        Process tsharkProcessBlackout = Runtime.getRuntime().exec("ssh -tt mmtr@10.254.7.106 '/usr/local/bin/tshark -Y \"tls.handshake.session_id && ip.dst == 10.254.7.106 && (ip.src == 193.27.224.36 || ip.src == 193.27.224.37 || ip.src == 193.27.224.38)\"'");
+//        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.getTsharkStartBlackout(CONFIG_FILE_URL));
+//        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
+//
+//        device.stepOk();
+//
+//        TimeUnit.SECONDS.sleep(SLEEP_TIME_STREAM);
+//
+//        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStream));
+//
+//        TimeUnit.SECONDS.sleep(SLEEP_TIME_BLACKOUT);
+//
+//        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessBlackout));
+//        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
+//
+//        device.stepCancelStream();
+//
+//        boolean isStreamStart = false;
+//        String strStream;
+//        while (tsharkProcessStreamReader.ready()) {
+//            strStream = tsharkProcessStreamReader.readLine();
+//            System.out.println(strStream); // tmp for test
+//            if (strStream.contains(START_STREAM_SERVER_MSG)) {
+//                isStreamStart = true;
+//                break;
+//            }
+//        }
+//
+//        List<String> blackoutList = new ArrayList<>(); // используется для Step 2
+//        String boLine;
+//        while (tsharkProcessBlackoutReader.ready()) {
+//            boLine = tsharkProcessBlackoutReader.readLine();
+//            if (boLine.contains(START_STREAM_SERVER_MSG)) {
+//                blackoutList.add(boLine.trim());
+//            }
+//        }
+//
+//        boolean existBlackout = blackoutList.isEmpty() ? false : true;
+//
+//        assertThat("C348_Step1: Видеопоток ОТСУТСТВУЕТ", isStreamStart, equalTo(true));
+//        assertThat("C348_Step1: Запрос на restrictions_api_url НЕ отправляется (блэкауты)", existBlackout, equalTo(true));
+//
+//        /******** Step 2 ********/
+//
+//        int boSuccessCount = 2;
+//        int secBoReqInterval = 15; // todo jsonConfigFile.getJSONObject("results").getJSONObject("sdk_config").get("restrictions_period_sec")
+//        int secBoReqLag = 2;
+//
+//        // 1) Должно быть еще 2 успешных запроса. (сам факт)
+//        boolean existTwoSuccessBoReq = false;
+//        // 2) Запросы отправляются каждые 15 секунд
+//        boolean isPeriodicityBoReq = true;
+//
+//        // tmp for test
+//        System.out.println("Блэкауты:");
+//        blackoutList.forEach(boString -> {
+//            System.out.println(boString);
+//        });
+//        // tmp for test
+//
+//        if (!blackoutList.isEmpty()) {
+//            int firstSecBoReq = getSecFromBoStr(blackoutList.get(0));
+//            Set<Integer> secFromBoStrSet = new HashSet<>();
+//            blackoutList.forEach(boString -> {
+//                int secFromBoStr = getSecFromBoStr(boString);
+//                if ((secFromBoStr - firstSecBoReq) <= (boSuccessCount * secBoReqInterval + secBoReqLag))
+//                    secFromBoStrSet.add(secFromBoStr);
+//            });
+//
+//            // 1)
+//            if (secFromBoStrSet.size() > boSuccessCount)
+//                existTwoSuccessBoReq = true;
+//
+//            // 2) Анализируем отправку только первых boSuccessCount запросов
+//            int indexBoString = 0;
+//            for (String boString : blackoutList) {
+//                if ((indexBoString > 0 && indexBoString <= boSuccessCount) && !(getSecFromBoStr(boString) >= (firstSecBoReq + secBoReqInterval * indexBoString - secBoReqLag) && getSecFromBoStr(boString) <= (firstSecBoReq + secBoReqInterval * indexBoString + secBoReqLag))) {
+//                    isPeriodicityBoReq = false;
+//                }
+//                indexBoString++;
+//            }
+//        }
+//
+//        assertThat("C348_Step2: Ещё двух запросов на блэкауты НЕТ", existTwoSuccessBoReq, equalTo(true));
+//        assertThat("C348_Step2: Периодичность отправки запросов НАРУШЕНА", isPeriodicityBoReq, equalTo(true));
+//    }
+//}
+//
