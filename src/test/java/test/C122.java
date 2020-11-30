df77b36b59d022128009e0c4ff9d8a8b6d44ed1b
dmitry_balalin <dbalalin@mmtr.ru>
2020-11-30 17:04:41 +0300

    первый степ с122первый степ с122

--- src/test/java/test/C122.java
+++ src/test/java/test/C122.java
@@ -32,7 +32,8 @@ public class C122 {
 
     @Test
     public void c122() throws IOException, InterruptedException {
-        device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
+//        device = "iPhone".equals(System.getenv("deviceType")) ? IosDevice.INSTANCE : AndroidDevice.INSTANCE;
+        device = "iPhone".equals(System.getenv("deviceType")) ? new IosDevice() : new AndroidDevice();
         JSONObject jsonConfigFile = readJsonFromUrl(CONFIG_FILE_URL);
 
         /******** Step 1 ********/
