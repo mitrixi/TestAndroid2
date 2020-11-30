4c736e6a818f49eb90ce252789475fc294ac77a8
Andrew Nekrasov <anekrasov@mmtr.ru>
2020-11-30 17:44:32 +0300

    iOS

--- src/test/java/test/C122.java
+++ src/test/java/test/C122.java
@@ -36,84 +36,84 @@ public class C122 {
         JSONObject jsonBlackout = readJsonFromUrl(urlBlackout);
         boolean broadcasting_allowed = Boolean.parseBoolean(jsonBlackout.getJSONArray("restrictions").getJSONObject(0).get("broadcasting_allowed").toString());
 
-        assertThat("C122_Step1 По ссылке в параметре конфига restrictions_api_url открывается jsonConfigFile-файл НЕ соответствующий описанию", broadcasting_allowed, equalTo(false));
+        assertThat("C122_Step1 По ссылке в параметре конфига restrictions_api_url открывается jsonConfigFile-файл НЕ соответствующий описанию", broadcasting_allowed, equalTo(true));
 
-//        /******** Step 2 ********/
+        /******** Step 2 ********/
+
+        device.stepToConfigUrl(CONFIG_FILE_URL);
+
+        int restrictionsPeriodSec = Integer.parseInt(jsonConfigFile.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_period_sec").toString());
+
+        Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
+        BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
+
+        device.stepOk();
+
+        TimeUnit.SECONDS.sleep(restrictionsPeriodSec * 4);
+
+        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStream));
+        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
+
+        device.stepCancelStream();
+
+        boolean isStreamStart = false;
+        String strStream;
+        while (tsharkProcessStreamReader.ready()) {
+            strStream = tsharkProcessStreamReader.readLine();
+            System.out.println(strStream);
+            if (strStream.contains(START_STREAM_SERVER_MSG)) {
+                isStreamStart = true;
+                break;
+            }
+        }
+
+//        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.getTsharkStartBlackout(CONFIG_FILE_URL));
+//        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
 //
-//        device.stepToConfigUrl(CONFIG_FILE_URL);
-//
-//        int restrictionsPeriodSec = Integer.parseInt(jsonConfigFile.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_period_sec").toString());
-//
-//        Process tsharkProcessStream = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
-//        BufferedReader tsharkProcessStreamReader = new BufferedReader(new InputStreamReader(tsharkProcessStream.getInputStream()));
-//
-//        device.stepOk();
-//
-//        TimeUnit.SECONDS.sleep(restrictionsPeriodSec * 4);
-//
-//        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStream));
-//        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
-//
-//        device.stepCancelStream();
-//
-//        boolean isStreamStart = false;
-//        String strStream;
-//        while (tsharkProcessStreamReader.ready()) {
-//            strStream = tsharkProcessStreamReader.readLine();
-//            System.out.println(strStream);
-//            if (strStream.contains(START_STREAM_SERVER_MSG)) {
-//                isStreamStart = true;
-//                break;
-//            }
+//        boolean existBlackout = false;
+//        if (tsharkProcessBlackoutReader.ready()) {
+//            existBlackout = true;
 //        }
-//
-////        Process tsharkProcessBlackout = Runtime.getRuntime().exec(device.getTsharkStartBlackout(CONFIG_FILE_URL));
-////        BufferedReader tsharkProcessBlackoutReader = new BufferedReader(new InputStreamReader(tsharkProcessBlackout.getInputStream()));
-////
-////        boolean existBlackout = false;
-////        if (tsharkProcessBlackoutReader.ready()) {
-////            existBlackout = true;
-////        }
-//
-//        boolean seeBlackout = device.seeBlackout();
-//
-//        assertThat("C122_Step2: Видеопоток отсутствует", isStreamStart, equalTo(true));
-//        assertThat("C122_Step2: Блэкаут НЕ виден", seeBlackout, equalTo(false));
-//
-//        /******** Step 3 ********/
-//
-//        device.allowBlackout();
-//
-//        device.stepToConfigUrl(CONFIG_FILE_URL);
-//
-//        int restrictionsPeriodSecStep3 = Integer.parseInt(jsonConfigFile.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_period_sec").toString());
-//
-//        Process tsharkProcessStreamStep3 = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
-//        BufferedReader tsharkProcessStreamReaderStep3 = new BufferedReader(new InputStreamReader(tsharkProcessStreamStep3.getInputStream()));
-//
-//        device.stepOk();
-//
-//        TimeUnit.SECONDS.sleep(restrictionsPeriodSecStep3 * 2);
-//
-//        boolean isBoOnScreenShot = device.isBoOnScreenShot(); // Блэкаут в приложении должен запуститься не позднее чем через <restrictions_period_sec>x2 секунд после перезапуска приложения.
-//
-//        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStreamStep3));
-//        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
-//
-//        device.stepCancelStream();
-//
-//        boolean isStreamStartStep3 = false;
-//        String strStreamStep3;
-//        while (tsharkProcessStreamReaderStep3.ready()) {
-//            strStreamStep3 = tsharkProcessStreamReaderStep3.readLine();
-//            System.out.println(strStreamStep3);
-//            if (strStreamStep3.contains(START_STREAM_SERVER_MSG)) {
-//                isStreamStartStep3 = true;
-//                break;
-//            }
-//        }
-//
-//        assertThat("C122_Step3: Видеопоток отсутствует", isStreamStartStep3, equalTo(true));
-//        assertThat("C122_Step3: Поверх видеотрансляции НЕ выводится заглушка блэкаута", isBoOnScreenShot, equalTo(true));
+
+        boolean seeBlackout = device.seeBlackout();
+
+        assertThat("C122_Step2: Видеопоток отсутствует", isStreamStart, equalTo(true));
+        assertThat("C122_Step2: Блэкаут НЕ виден", seeBlackout, equalTo(false));
+
+        /******** Step 3 ********/
+
+        device.allowBlackout();
+
+        device.stepToConfigUrl(CONFIG_FILE_URL);
+
+        int restrictionsPeriodSecStep3 = Integer.parseInt(jsonConfigFile.getJSONObject("result").getJSONObject("sdk_config").get("restrictions_period_sec").toString());
+
+        Process tsharkProcessStreamStep3 = Runtime.getRuntime().exec(device.getTsharkStartFilePath());
+        BufferedReader tsharkProcessStreamReaderStep3 = new BufferedReader(new InputStreamReader(tsharkProcessStreamStep3.getInputStream()));
+
+        device.stepOk();
+
+        TimeUnit.SECONDS.sleep(restrictionsPeriodSecStep3 * 2);
+
+        boolean isBoOnScreenShot = device.isBoOnScreenShot(); // Блэкаут в приложении должен запуститься не позднее чем через <restrictions_period_sec>x2 секунд после перезапуска приложения.
+
+        Runtime.getRuntime().exec("kill -9 " + getPidOfProcess(tsharkProcessStreamStep3));
+        Runtime.getRuntime().exec(device.getTsharkStopFilePath());
+
+        device.stepCancelStream();
+
+        boolean isStreamStartStep3 = false;
+        String strStreamStep3;
+        while (tsharkProcessStreamReaderStep3.ready()) {
+            strStreamStep3 = tsharkProcessStreamReaderStep3.readLine();
+            System.out.println(strStreamStep3);
+            if (strStreamStep3.contains(START_STREAM_SERVER_MSG)) {
+                isStreamStartStep3 = true;
+                break;
+            }
+        }
+
+        assertThat("C122_Step3: Видеопоток отсутствует", isStreamStartStep3, equalTo(true));
+        assertThat("C122_Step3: Поверх видеотрансляции НЕ выводится заглушка блэкаута", isBoOnScreenShot, equalTo(true));
     }
 }
