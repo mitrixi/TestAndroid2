9ae0195b69ca4af6c69a051d85c2a9a5594e5ab1
Andrew Nekrasov <anekrasov@mmtr.ru>
2020-12-04 16:20:41 +0300

    iOs

--- src/test/java/device/DeviceFactory.java
+++ src/test/java/device/DeviceFactory.java
@@ -6,6 +6,7 @@ import device.version.ios.I6_V4_2_5;
 import java.util.function.Supplier;
 
 public enum DeviceFactory {
+    // deviceVersion енума (например: "I6_V4.2.5") должно соответствовать jenkins параметру DeviceVersion (см. General-"параметризованная сборка")
     I6_V425("I6_V4.2.5", I6_V4_2_5::getInstance),
     A9_V431("A9_V4.3.1", A9_V4_3_1::new);
 
