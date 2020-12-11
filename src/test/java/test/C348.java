ec0605ef006f2e01dd197f3d73fc84345287edfd
Andrew Nekrasov <anekrasov@mmtr.ru>
2020-12-11 15:20:47 +0300

    iOs

--- src/test/java/test/C348.java
+++ src/test/java/test/C348.java
@@ -49,7 +49,7 @@ public class C348 {
 //        }
 //        /**/
 
-        Process tsharkProcessStream0 = Runtime.getRuntime().exec("ssh -tt mmtr@10.254.7.106 '/usr/local/bin/tshark'");
+        Process tsharkProcessStream0 = Runtime.getRuntime().exec("ssh -tt mmtr@10.254.7.106 '/usr/local/bin/tshark -Y \"(ip.src != 92.223.99.99 || ip.src == 178.176.158.68 || ip.src == 178.176.158.69 || ip.src == 195.161.167.68 || ip.src == 195.161.167.69) && ip.dst == 10.254.7.106\"'");
         /**/
         BufferedReader tsharkProcessStreamReader0 = new BufferedReader(new InputStreamReader(tsharkProcessStream0.getInputStream()));
 
